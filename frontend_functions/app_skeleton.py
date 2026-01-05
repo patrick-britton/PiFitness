280242040541b672128a6c4d3820132dd892a065
rickb <rick.britton@gmail.com>
2026-01-05 15:26:29 -0800

    Updated music module with playlist configuration editor.

--- frontend_functions/app_skeleton.py
+++ frontend_functions/app_skeleton.py
@@ -4,6 +4,7 @@ from backend_functions.helper_functions import reverse_key_lookup
 from backend_functions.logging_functions import log_app_event, start_timer, elapsed_ms
 from frontend_functions.admin_module import render_admin_module
 from frontend_functions.homepage import render_homepage
+from frontend_functions.music_module import render_music
 from frontend_functions.nav_buttons import nav_button
 from frontend_functions.streamlit_helpers import ss_debug
 
@@ -41,7 +42,7 @@ def render_skeleton():
     elif nav_selection == 'admin':
         render_admin_module()
     elif nav_selection == 'music':
-        st.info('MUSIC NOT YET BUILT')
+        render_music()
     elif nav_selection == 'running':
         st.info('RUNNING NOT YET BUILT')
     elif nav_selection == 'food':
