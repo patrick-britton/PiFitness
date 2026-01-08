4f4165b2b73f6d480ac2609e8adefc1c0ec78dc9
rickb <rick.britton@gmail.com>
2026-01-07 16:42:58 -0800

    Bugfix for array interpreted as string.

--- frontend_functions/admin_module_widgets.py
+++ frontend_functions/admin_module_widgets.py
@@ -67,8 +67,8 @@ def task_execution_chart():
                                                            y_max=max_elt)}
 
     st.write(f"__{task_count}__ tasks with history in last 30 days")
-    if isinstance(df['etl_time_history'].iloc[0], str):
-        df['etl_time_history'] = df['etl_time_history'].apply(
+    if isinstance(df['etl_time_s'].iloc[0], str):
+        df['etl_time_s'] = df['etl_time_s'].apply(
             lambda x: [float(i) for i in x.replace('{', '').replace('}', '').split(',')]
         )
     st.dataframe(data=df,
